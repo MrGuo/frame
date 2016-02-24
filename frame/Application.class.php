e5e0ca37fcc262d1aa4f27b26c4c8a04af141ba0
smallwalk <2232445050@qq.com>
2016-02-24 11:46:23 +0800

    remove note

--- frame/Application.class.php
+++ frame/Application.class.php
@@ -61,7 +61,6 @@ class Application {
     public function run() {
         $this->setRequireMent();
         $this->middleware[0]->call();
-        //修改一个东东（主干）
     }
 
     private function setRequireMent() {
