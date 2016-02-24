74f9c38d5d0d9440482b50d5dcad69582212cd8e
MrGuo <853089062@qq.com>
2016-02-24 12:10:38 +0800

    confilct

--- frame/Application.class.php
+++ frame/Application.class.php
@@ -61,7 +61,7 @@ class Application {
     public function run() {
         $this->setRequireMent();
         $this->middleware[0]->call();
-        //
+        //一个
     }
 
     private function setRequireMent() {
